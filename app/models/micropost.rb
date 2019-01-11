124f35497c10226921c958bf91c1d526ac5a0166
gali <gbmatrix@gmail.com>
2019-01-11 17:53:12 +0200

    added dependencies and validations

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,2 +1,8 @@
 class Micropost < ApplicationRecord
+
+    belongs_to :user
+
+    validates :content, presence: true, length: {maximum: 140, minimum: 3}
+    validates :user_id, presence: true
+
 end
